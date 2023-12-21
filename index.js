28c11afe6aa5f0fb39d93097e0f5c47c45b6e36b
Ayush <ayushphenomal1@gmail.com>
2023-12-22 00:00:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var hamburger = document.getElementById("drop")
+
+
+
+function myFunction() {
+    if (hamburger.style.display == "block") {
+   
+        hamburger.style.display = "none";
+    }
+    else {
+        hamburger.style.display = "block";
+    }
+}
+
+
+
+
+
